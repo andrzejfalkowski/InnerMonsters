368ecb99e8149141ab42f7cbb8bcd8b358913ec3
Diego de Palacio Ruiz Caba <diego@Diegos-MacBook-Air.local>
2015-08-22 15:28:47 +0200

    First UI functionality

--- InnerMonsters/Assets/Scripts/Floor.cs
+++ InnerMonsters/Assets/Scripts/Floor.cs
@@ -1,10 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum Dir { N, E, S, W, NONE }
+
 public class Floor : MonoBehaviour 
 {
 	public PersonOfInterest Person;
 	public PickableObject Pickable;
+	public Floor[] nextFloors = new Floor[4];
+
+	public float GetDistanceToFloor( Dir dir )
+	{
+		switch( dir )
+		{
+			case Dir.N: return nextFloors[ (int)Dir.N ].transform.position.y - transform.position.y; break;
+			case Dir.E: return nextFloors[ (int)Dir.E ].transform.position.x - transform.position.x; break;
+			case Dir.S: return transform.position.y - nextFloors[ (int)Dir.S ].transform.position.y; break;
+			case Dir.W: return transform.position.x - nextFloors[ (int)Dir.W ].transform.position.x; break;
+		}
+		return 0.0f;
+	}
 
 	// Use this for initialization
 	void Start () {
